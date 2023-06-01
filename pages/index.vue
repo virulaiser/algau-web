98b784defb065508ea71ae206fb1d3f02018cbf9
virulaiser <jsiutto@gmail.com>
2023-06-01 15:37:16 -0300

    footer_corregido

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
     <b-container fluid>
+      <h1 class="text-center">Nueva Pagina</h1>
+        <p> Les damos la bienvenida a la nueva página de Algau , otra forma de vincularnos ¡!
+Con el correr de los días, iremos actualizando información de temas relevantes que pueden ser importantes para nuestros socios y público en general. Pedimos paciencia y recibimos sus consultas de la pagina a consultas.algau@gmail.com  y temas de interés de la pagina.</p><p> Tanto de públicos, como de privados, lo mismo de cualquier sponsor que nos puedan ayudar, dado que la asociación se financia por medio propio y actualmente no cobra anualidad, pero creemos firmemente en la colaboración de todos los Licenciados. Desde ya Muchas gracias por visitarnos, cada poco día iremos agregando noticias.</p> <p>Si SOS licenciado recibido, podes unirte a nuestro WhatsApp que compartimos, información, ofrecimientos de trabajos, información en general y particular de cada rubro, </p>
 
+<b-container fluid>
 
-      
+        <div id="carouselExampleSlidesOnly" class="carousel slide text-center" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="molino.jpg" class="d-block w-75" alt="molino">
+              </div>
+              <div class="carousel-item">
+                <img src="vaca1.jpg" class="d-block w-75" alt="vaca">
+              </div>
+              <div class="carousel-item">
+                <img src="riego.jpeg" class="d-block w-75" alt="riego">
+              </div>
+       
+      </div></div>
+    </b-container>   
 
   
   </b-container>
@@ -20,3 +37,8 @@ export default {
 
 }
 </script>
+<style>
+p{font-size:0.8rem;}
+h1{font-size:1.5rem}
+
+</style>
